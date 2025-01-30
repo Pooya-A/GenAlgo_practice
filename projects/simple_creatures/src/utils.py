3289c8b68ad97401cc1234b4615586a43ff2092d
Pooya-A <30982157+Pooya-A@users.noreply.github.com>
2025-01-30 23:54:05 +0000

    rework plotting function

--- projects/simple_creatures/src/utils.py
+++ projects/simple_creatures/src/utils.py
@@ -12,21 +12,7 @@ def get_run_df(creatures):
 	return df.sort_values(by='score', ascending=False)
 
 
-def plot_initial_step(creatures, foods, x_width, y_width):
-	plt.figure(figsize=(5,5))
-
-	plt.xlim(0,x_width)
-	plt.ylim(0,y_width)
-	for i in creatures:
-		plt.scatter(i.x,i.y, c='r')
-		plt.plot([i.x, i.x + np.cos(i.orientation)*i.speed], [i.y,i.y+np.sin(i.orientation)*i.speed], c='k')
-	
-	for key, item in foods.items():
-		plt.scatter(item.x,item.y, c='g')
-	return
-
-
-def plot_a_step(creatures, foods,x_width, y_width):
+def plot_state(creatures, foods, x_width, y_width):
 	clear_output(wait=True)
 	fig = plt.figure(figsize=(5,5))
 	ax = plt.axes()
